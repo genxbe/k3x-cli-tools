de2a3cb3f0558c2b5815b9db67e208bccb713d3a
Sam Serrien <sam@genx.be>
2022-10-18 19:50:08 +0200

    Plugins helper

---
+++ classes/Support/Plugins.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace X\Support;
+
+use Kirby\Cms\Plugin;
+use X\Support\Menu\Menu;
+
+class Plugins
+{
+	private static string $result;
+
+	public static function get(): array
+	{
+		$sys = new \Kirby\Cms\System(kirby());
+
+        $installedPlugins = [];
+
+        foreach($sys->plugins() as $plugin)
+        {
+            array_push($installedPlugins, $plugin);
+        }
+
+        if(empty($installedPlugins))
+        {
+            H::error('ABORTED', 'There are no plugins installed.');
+            die();
+        }
+
+		ksort($installedPlugins);
+
+		return $installedPlugins;
+	}
+
+	public static function select($title = 'Select a plugin'): Plugin
+    {
+		$installedPlugins = self::get();
+
+		$option = (new Menu(
+			title: $title,
+			options: $installedPlugins,
+			askForConfirmation: true,
+		))->open();
+
+		if(is_null($option))
+		{
+			H::error('ABORTED', 'No plugin selected or process aborted.');
+			die();
+		}
+
+        return $installedPlugins[$option];
+    }
+}
